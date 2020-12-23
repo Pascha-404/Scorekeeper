66efc81ab899aa732cb5f9b921240afbbc454059
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2020-12-24 00:54:28 +0100

    First Files

---
+++ app.js
@@ -0,0 +1,12 @@
+const playerOne = document.querySelector('.player1');
+const playerTwo = document.querySelector('.player2');
+
+let scoreOne = 0;
+let scoreTwo = 0;
+
+const maxPoints = document.querySelector('#maxPoints')
+
+const btn1 = document.querySelector('#btnPl1')
+const btn2 = document.querySelector('#btnPl2')
+const btnRes = document.querySelector('#btnReset')
+
